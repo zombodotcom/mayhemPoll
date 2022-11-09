f00abb06a3e81b8fa35a13c91975ec0e9de736b8
zombodotcom <tommysciano@gmail.com>
2022-11-08 18:12:14 -0700

    smaller version

--- server.js
+++ server.js
@@ -268,6 +268,8 @@ let maps = [
 // All of the users
 const users = {}
 
+var users_count = 0;
+
 // Cool down in milliseconds
 const coolDown = 2000
 
@@ -277,6 +279,11 @@ const magicNumber = 25381238823847823427345
 // On new client connection
 io.on("connection", (socket) => {
     socket.emit("mapUpdate", maps);
+    //on connection, update users count
+    ++users_count;
+    //send it to every opened socket
+    io.sockets.emit('users_count', users_count);
+    console.log(users_count)
 
     // Parse the user agent string
     const userAgent = useragent.parse(socket.handshake.headers["user-agent"]);
